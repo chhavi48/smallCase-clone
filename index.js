847bf941e908bf26aa9efc4fa526b1923cd62c54
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-03-31 10:33:41 +0530

    smallcasehome
    smallcase clone

---
+++ index.js
@@ -0,0 +1,10 @@
+function text() {
+    para.innerText=null;
+    document.getElementById("para").style.color = "red";
+    // var div= document.createElement('div')
+    // var para=document.createElement('p')
+    // para.innerText="smallcases are modern investment products that help you build a low-cost, long-term & diversified portfolio easily. Created by professionals, each smallcase is a ready-made basket of stocks/ETFs that reflects a theme, strategy or objective. Watch our explainer video here."
+    // div.append(para)
+    document.getElementById("para").innerText="smallcases are modern investment products that help you build a low-cost, long-term & diversified portfolio easily. Created by professionals, each smallcase is a ready-made basket of stocks/ETFs that reflects a theme, strategy or objective. Watch our explainer video here."
+  }
+  
