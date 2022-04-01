c930d8fa82622f5ac430684eb9267b8225a324eb
MOHAMMAD ARIF <md970824@gmail.com>
2022-04-01 22:47:36 +0530

    edited code

---
+++ Md Arif/scripts/chhavi.js
@@ -0,0 +1,210 @@
+function chhavi(){
+   return `<div class="row">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-5 fonts">
+        <h1 style="font-weight: 100;">A smallcase is a basket of <br>stocks that reflects an idea</h1>
+        <p>smallcases are portfolios of stocks or ETFs, weighted<br> intelligently to track a theme, strategy or objective</p>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/CUAg2SnW1xI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div class="details">
+        <div class="forimg">
+        <img src="https://ik.imagekit.io/smallcase/wp-content/uploads/2018/07/smallcases-bp-2.jpg?tr=w-176,h-152,fo-auto" height="80px"/>  
+        </div>
+        <p style="margin-left: 4%;"> Investing Insights<br>
+Why Investors Should Invest in smallcases</p>
+    </div>
+ </div>
+    <div class="col-sm-6">
+        <div class="left">
+            <div class="leftimage">
+                <img src="https://www.smallcase.com/static/images/svg/what-is-smallcase/1.svg"/>
+            </div>
+            <div class="leftdetails">
+         <p style="margin-top: 5%; font-size: 22px; font-weight: bold;">Simple to understand<p>
+          <p class="fonts">smallcases are modern investing products <br>based on simple ideas you can understand.</p>
+         </div>
+         </div>
+
+         <div class="left">
+             <div class="leftimage">
+                 <img src="https://www.smallcase.com/static/images/svg/what-is-smallcase/2.svg"/>
+             </div>
+             <div class="leftdetails">
+          <p style="margin-top: 5%; font-size: 22px; font-weight: bold; ">Simple to understand<p>
+           <p class="fonts">smallcases are created by India’s leading<br> finance experts & backed by solid research.</p>
+          </div>
+          </div>
+
+          <div class="left">
+             <div class="leftimage">
+                 <img src="https://www.smallcase.com/static/images/svg/what-is-smallcase/3.svg"/>
+             </div>
+             <div class="leftdetails">
+          <p style="margin-top: 5%; font-size: 22px; font-weight: bold; ">
+             Designed for you<p>
+           <p class="fonts">smallcases are fully customizable. Edit your <br>smallcase constituents any time or create your<br> own smallcase.</p>
+          </div>
+          </div>
+
+    </div>
+</div>
+
+<div class="row marignsize">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-5 fonts">
+     <h2 >Curated by India's <br>top investment managers</h2>
+     smallcases are created & managed by leading professionals who are SEBI registered research analysts and investment advisors
+    </div>
+    <div class="col-sm-1"></div>
+    <div class="col-sm-5">
+        <video src="https://assets.smallcase.com/smallcase/assets/videos/creator-scroll.mp4" autoplay width="500px"></video>
+    </div>
+</div>
+
+<div class="row marignsize">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-5">
+    <p> Everything you need to invest</p>
+
+         <h2>Simple, quick & delightful <br>investing for you</h2>
+
+      <div class="rightdetails">
+         <p style="font-size: 18px; font-weight: bold;"><i class="fa-solid fa-thumbs-up"></i>Buy multiple stocks in 1 click</p>
+            <p>Place orders for up to 50 stocks in 1 click as a smallcase and save time</p> 
+      </div>   
+      <div class="rightdeatils1" >
+          <p>Track smallcases anytime, anywhere</p>
+      </div>
+      <div class="rightdeatils2">
+         <p>Invest regularly with SIPs</p>
+     </div>
+     <div class="rightdeatils3">
+         <p>Create your own smallcase</p>
+     </div>
+
+   
+    </div>
+    <div class="col-sm-1"></div>
+    <div class="col-sm-4">
+        <video src="https://assets.smallcase.com/smallcase/assets/videos/invest_new.mp4" autoplay  class="mobile"  width="300px" height="500px"></video>
+    </div>
+</div>
+
+<div class="row marignsize">
+    <div class="col-sm-12">
+        <p style="text-align: center;">As soon on</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-2">
+        <img src="https://www.smallcase.com/static/images/svg/forbes.svg"/>
+    </div>
+    <div class="col-sm-2">
+        <img src="https://www.smallcase.com/static/images/svg/fortune.svg"/>
+    </div>
+    <div class="col-sm-2">
+        <img src="https://www.smallcase.com/static/images/svg/livemint.svg"/>
+    </div>
+    <div class="col-sm-2">
+        <img src="https://www.smallcase.com/static/images/svg/moneycontrol.svg"/>
+    </div>
+    <div class="col-sm-2">
+        <img src="https://www.smallcase.com/static/images/svg/cnbc.svg" height="50px"/>
+    </div>
+    <div class="col-sm-1"></div>
+</div>
+
+<div class="row marignsize">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-10 background">
+
+        <h2 style="text-align: center;color: white; margin-top: 10%;">We work with India's</br>
+         most trusted financial brands</h2>
+         <p style="color: white; text-align: center; font-size: 20px;"> When you invest in smallcases, stocks you buy are held in your <br>existing demat account. Get started with your trading account</p>
+       
+         <div class="row">
+             <div class="col-sm-3">
+             </div>
+             <div class="col-sm-6">
+         <div class="shift">
+           <div class="bgimage">
+               <img src="https://assets.smallcase.com/smallcase/assets/brokerLogo/big/angelbroking.png" alt=""  height="80px" width="100px">
+           </div>
+           <div class="bgimage">
+             <img src="https://assets.smallcase.com/smallcase/assets/brokerLogo/big/hdfc.png" alt=""  height="50px" width="100px" style="margin-top:10%; margin-left: 2%;">
+         </div>
+         <div class="bgimage">
+             <img src="https://assets.smallcase.com/smallcase/assets/brokerLogo/big/icici.png" alt=""  height="50px" width="100px">
+         </div>
+         </div>
+
+         <div class="shift2">
+             <div class="bgimage">
+                 <img src="https://assets.smallcase.com/smallcase/assets/brokerLogo/big/kotak.png" alt=""  height="80px" width="100px">
+             </div>
+             <div class="bgimage">
+               <img src="https://assets.smallcase.com/smallcase/assets/brokerLogo/big/upstox.png" alt=""  height="50px" width="100px" style="margin-top:10%; margin-left: 2%;">
+           </div>
+           <div class="bgimage">
+               <img src="https://assets.smallcase.com/smallcase/assets/brokerLogo/big/kite.png" alt=""  height="50px" width="100px" style="margin-top:10%; margin-left: 2%;">
+           </div>
+           </div>
+
+           <button class="btn"><a href="https://www.smallcase.com/">see all Broker</a></button>
+             </div>
+
+             <div class="col-sm-3"></div>
+         </div>
+       </div>
+         <div class="col-sm-1"></div>
+
+
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-12 marignsize">
+        <h3 style="text-align: center;">Frequently Asked Questions</h3>
+    </div>
+
+</div>
+<div class="row">
+ <div class="col-sm-1"></div>
+ <div class="col-sm-4 font">
+
+     <p>Getting Started</p>
+     <p  onclick="text()" style="margin-top: 10%;">What are smallcase?</p>
+     <p>How to invest in SmallCase?</p>
+     <p>Fees and Taxes</p>
+     <p>Can i buy smallcase as SIP?</p>
+     <div id="para"></div>
+ </div>
+ <div class="col-sm-3 font">
+     <p style="margin-top: 10%;">Do you get dividends from smallcases?</p>
+     <p>What is rebalencing?</p>
+     <p>Who manage SmallCases?</p>
+ </div>
+ <div class="col-sm-3 font">
+     <p>Investing with SmallCases</p>
+     <p style="margin-top: 10%;">WatchList And Draft</p>
+     <p>Creating Your Profile</p>
+     <p>Order and History</p>
+     <p>Tracking a profile of stock</p>
+ </div>
+ <div class="col-sm-1"></div>
+</div>
+
+<div class="row marignsize">
+    <div class="col-sm-4"></div>
+    <div class="col-sm-4 fonts">
+        <p style="text-align: center; font-size: 18px;">Have more questions or some feedback?</p>
+         <p style="text-align: center;">We are just an email away</p>
+         <button class="btn2"><a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=help@smallcase.com">Write to Us</a></button>
+    <p style="margin-left: 30%;margin-top: 4%;">Or visit our <a href="https://help.smallcase.com/en/">Help Center</a></p>
+     </div>
+
+    <div class="col-sm-4"></div>
+</div>`;
+}
+
+export default chhavi;
---
+++ Md Arif/scripts/popup.js
@@ -0,0 +1,47 @@
+function popup1(){
+    return `<div id="brand">
+    <div>
+        <img src="https://assets.smallcase.com/images/smallcases/200/SCNM_0007.png" style="width:60px;"/>
+
+    </div>
+    <div>
+        <h2>Brand Value</h2>
+        <p>
+            Invest in India’s growing love for branded products</p>
+    </div>
+    <div id="cager">
+        <div><p>6Y CAGER</p>    
+        <h4>16.55%</h4>
+        </div>
+        <button id="invest">Invest Now</button>
+    </div>
+    
+</div>`;
+}
+
+function popup2(){
+
+    return `<div id="brand">
+    <div>
+        <img src="https://assets.smallcase.com/images/smallcases/200/SCAW_0001.png" style="width:60px;"/>
+
+    </div>
+    <div>
+        <h2>
+            All Weather Investing</h2>
+        <p>
+            
+      All Weather Investing
+      Build a foundation with a portfolio of equity, gold & fixed income ETFs</p>
+    </div>
+    <div id="cager">
+        <div><p>4Y CAGER</p>    
+        <h4>12.54%</h4>
+        </div>
+        <button id="invest">Invest Now</button>
+    </div>
+    
+</div>`;
+}
+
+export {popup1,popup2};
